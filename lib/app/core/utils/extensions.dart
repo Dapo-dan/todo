4bc79d1f76c12ead3d29d7387b8847596015b52f
Dapo <odapo138@gmail.com>
2022-04-05 11:18:12 +0100

    completed task card widget

--- lib/app/core/utils/extensions.dart
+++ lib/app/core/utils/extensions.dart
@@ -17,7 +17,10 @@ extension HexColor on Color {
     buffer.write(hexString.replaceFirst('#', ''));
     return Color(int.parse(buffer.toString(), radix: 16));
   }
-}
 
-// String toHex({bool leadingHashSign = true}) => '${leadingHashSign}';
-// '${alpha.toRadix}'
+  String toHex({bool leadingHashSign = true}) => '$leadingHashSign'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
+}
--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:todo/app/data/models/task.dart';
 import 'package:todo/app/data/services/storage/repository.dart';
@@ -5,19 +6,36 @@ import 'package:todo/app/data/services/storage/repository.dart';
 class HomeController extends GetxController {
   TaskRepository taskRepository;
   HomeController({required this.taskRepository});
-
+  final formKey = GlobalKey<FormState>();
+  final editController = TextEditingController();
+  final chipIndex = 0.obs; // Default index of zero
   final tasks = <Task>[].obs; // List of tasks and observe whenever it changes
 
   @override
   void onInit() {
     super.onInit();
     tasks.assignAll(taskRepository.readTasks());
-    ever(tasks, (_) => taskRepository.writeTasks(tasks)); // Whenever the task changes, this method is called
+    ever(
+        tasks,
+        (_) => taskRepository.writeTasks(
+            tasks)); // Whenever the task changes, this method is called
   }
 
   @override
   // ignore: unnecessary_overrides
   void onClose() {
     super.onClose();
+  }
+
+  void changeChipIndex(int value) {
+    chipIndex.value = value;
+  }
+
+  bool addTask(Task task) {
+    if (tasks.contains(task)) {
+      return false;
+    }
+    tasks.add(task);
+    return true;
   }
 }
